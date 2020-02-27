7f53d726e31c3d5644ab9d030fab2bfa7aa0da1c
Yogesh <yogesh@Yogeshs-MacBook-Air.local>
2020-02-27 23:03:58 +0530

    Updates

---
+++ precache-manifest.758d835fb4e0b398fc14036d0deefc7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc231238a53128c9286212a77ea57db",
+    "url": "/yo_stories/index.html"
+  },
+  {
+    "revision": "e8a21e64e14a93561b26",
+    "url": "/yo_stories/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "88c11d0a518701ab3f5f",
+    "url": "/yo_stories/static/js/2.784c21c7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/yo_stories/static/js/2.784c21c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a21e64e14a93561b26",
+    "url": "/yo_stories/static/js/main.8da92f25.chunk.js"
+  },
+  {
+    "revision": "975d42e700fe31d53c4b",
+    "url": "/yo_stories/static/js/runtime-main.8a3b9f33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yo_stories/static/media/logo.5d5d9eef.svg"
+  }
+]);
